9429ffc2a56abb6532782b1416af96046819783b
Visa-Valtteri Pimiä <vp79799@gmail.com>
2021-01-17 22:52:51 +0200

    unity base with player physics in 2d, naming, multiplayer sync over Mirror api, basic player model

---
+++ Assets/Mirror/JlanSus/Scripts/JlanNetworkManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using TMPro;
+
+namespace Mirror.JlanSus
+{
+    [AddComponentMenu("")]
+    public class JlanNetworkManager : NetworkManager
+    {
+        public RectTransform playerSpawn;
+
+        GameObject GetChildWithName(GameObject obj, string name) 
+        {
+            Transform trans = obj.transform;
+            Transform childTrans = trans.Find(name);
+            if (childTrans != null) {
+                return childTrans.gameObject;
+            } else {
+                return null;
+            }
+        }
+
+        public override void OnServerAddPlayer(NetworkConnection conn)
+        {
+            // add player at correct spawn position
+            var s = playerSpawn;
+            
+            GameObject player = Instantiate(
+                playerPrefab, 
+                new Vector3(Random.Range(s.rect.xMin, s.rect.xMax), Random.Range(s.rect.yMin, s.rect.yMax), 0) + s.transform.position, 
+                Quaternion.identity);
+
+            player.layer = 9; // players layer
+
+            NetworkServer.AddPlayerForConnection(conn, player);
+
+//                ball = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Ball"));
+//                NetworkServer.Spawn(ball);
+        }
+
+        public override void OnServerDisconnect(NetworkConnection conn)
+        {
+//            if (ball != null)
+//                NetworkServer.Destroy(ball);
+
+            // call base functionality (actually destroys the player)
+            base.OnServerDisconnect(conn);
+        }
+    }
+}
