83a8a613c4cc033e4e6bc99dcba325e9d8466448
Visa-Valtteri Pimiä <vp79799@gmail.com>
2021-01-21 23:00:49 +0200

    game state display

---
+++ Assets/Mirror/JlanSus/Scripts/GameStateTextUpdater.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Mirror.JlanSus 
+{
+    public class GameStateTextUpdater : MonoBehaviour
+    {
+        public GameManager gameManager;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText(Enum.GetName(typeof(GameState), gameManager.CurrentState));
+        }
+    }
+}
